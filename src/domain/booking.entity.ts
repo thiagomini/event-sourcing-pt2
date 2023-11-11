5a7701bab3c318b0ba0e988417d2647a44bd90c7
Thiago Martins <thiago_valentim1@hotmail.com>
2023-11-11 18:04:53 -0300

    feat(domain): add booking entity and status

--- src/domain/booking.entity.ts
+++ src/domain/booking.entity.ts
@@ -1,4 +1,7 @@
+import { BookingStatus } from './booking.status.js';
+
 export class Booking {
   public readonly customerId: string;
   public readonly hotelId: string;
+  public readonly bookingStatus: BookingStatus;
 }
---
+++ src/domain/booking.status.ts
@@ -0,0 +1,6 @@
+export enum BookingStatus {
+  Booked,
+  Confirmed,
+  Paid,
+  Cancelled,
+}
